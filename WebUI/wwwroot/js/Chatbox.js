3d075b6cc795ffec1c8fb8c883deeb29ba3e3824
Hamza <hamzamrbek@gmail.com>
2025-01-02 02:04:27 +0200

    Make the contacts container able to open and close.

--- WebUI/wwwroot/js/Chatbox.js
+++ WebUI/wwwroot/js/Chatbox.js
@@ -5,3 +5,10 @@ SideMenu = document.getElementById('SideMenu');
 toggleBtn.addEventListener('click', () => {
     SideMenu.classList.toggle('active');
 });
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('contact');
+    sidebar.classList.toggle('open');
+    const chat = document.getElementById("chat");
+    chat.classList.toggle('hide');
+}
